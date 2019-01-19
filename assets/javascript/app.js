2b69823677a78b020cb59508ffe8a9385558b44a
CW Wolf <44934371+ethancampbell77@users.noreply.github.com>
2019-01-19 18:37:41 -0500

    2nd Commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,56 @@
+// Javascript & JQuery //
+
+var card = $("#gameBox");
+
+var questions = [
+{
+    question: "What band first recorded the iconic song “Motorhead”?",
+    answers: ["Motorhead", "The Stooges", "Budgie", "Hawkwind"],
+    rightAnswer: "Hawkwind",
+},
+{
+    question: "Before his time in The Ramones, Mark Bell (A.K.A. Marky Ramone) played drums in which Proto-Metal power trio?",
+    answers: ["Sir Lord Baltimore", "Dust", "Overkill", "Black Sabbath"],
+    rightAnswer: "Dust",
+},
+{
+    question: "What was the name of Iggy Pop's Pre-Stooges Garage Rock band?",
+    answers: ["The Stoogettes", "Johnny and the Things", "The Iguanas", "Barry and the Remains"],
+    rightAnswer: "The Iguanas",
+  },
+  {
+    question: "Which band did Dave Mustaine play in prior to forming Megadeth?",
+    answers: ["Exodus", "Anthrax", "Metallica", "Slayer"],
+    rightAnswer: "Metallica",
+  },
+  {
+    question: "What 60’s Psych band was briefly known as 'The Loudest Band in the World'?",
+    answers: ["Blue Cheer", "MC5", "13th Floor Elevators", "The A-Bones"],
+    rightAnswer: "Blue Cheer",
+  },
+  {
+    question: "Which band recorded the Punk Rock masterpiece Los Angeles?",
+    answers: ["The Ramones", "The Clash", "The Germs", "X"],
+    rightAnswer: "X",
+  },
+  {
+    question: "Pretty Things drummer, John Charles Edward “Twink” Alder went on to record with which London-based Punk band?",
+    answers: ["Sex Pistols", "The Rings", "The Exploited", "Public Image Ltd"],
+    rightAnswer: "The Rings",
+  },
+  {
+    question: "Before she “Loved Rock ’N Roll”, Joan Jett played guitar in which all-girl band?",
+    answers: ["The Shaggs", "Shonen Knife", "The Runaways", "Hootie and the Blowfish"],
+    rightAnswer: "The Runaways",
+  },
+  {
+    question: "What was the name of Phil Lynott’s band before forming Thin Lizzy?",
+    answers: ["Bon Jovi", "Skid Row", "Taste", "Eden’s Children"],
+    rightAnswer: "Skid Row",
+  },
+  {
+    question: "Early in his career, Neil Young played in a band with which fellow Canadian musician?",
+    answers: ["Rick James", "Geddy Lee", "Bryan Adams", "Alanis Morissette"],
+    rightAnswer: "Rick James",
+  },
+];
